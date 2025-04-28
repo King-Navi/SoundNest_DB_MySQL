2c43f40f78c85539c67e04315e06643cdf10d558
U Náay Loeza <120292439+unaay20@users.noreply.github.com>
2025-04-28 12:24:17 -0600

    Update 02-init-data.sql inserted data

--- 02-init-data.sql
+++ 02-init-data.sql
@@ -1,2 +1,3 @@
 -- default roles
 INSERT INTO `Role` (`nameRol`) VALUES ('listener'), ('moderator');
+INSERT INTO soundnest.AppUser (idUser, nameUser, password, email, idRole) VALUES ('1', 'unaay20', '$2a$12$JBU0e5bDpDjdhNipE.k.a.hIXReLMe3qcuJ0h3GJZGNvlVi7DlGti', 'unaayjose@gmail.com', '2');
