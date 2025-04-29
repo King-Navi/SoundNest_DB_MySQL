01ba5eb578023ef690fc2bc181fdb76f334bc773
Navi <168604098+King-Navi@users.noreply.github.com>
2025-04-29 09:48:04 -0600

    Correction of comment

--- 02-init-data.sql
+++ 02-init-data.sql
@@ -3,6 +3,6 @@ INSERT INTO `Role` (`nameRol`) VALUES ('listener'), ('moderator');
 
 
 
---U
+-- U
 INSERT INTO soundnest.AppUser (nameUser, password, email, idRole)
 VALUES ('unaay20', '$2a$12$JBU0e5bDpDjdhNipE.k.a.hIXReLMe3qcuJ0h3GJZGNvlVi7DlGti', 'unaayjose@gmail.com', '2');
