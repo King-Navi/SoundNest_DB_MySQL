fc893d52f525484edb6c514f4d276fad70dc5cb7
Navi <168604098+King-Navi@users.noreply.github.com>
2025-05-04 20:31:32 -0600

    more data & final database

--- 01-init.sql
+++ 01-init.sql
@@ -1,14 +1,42 @@
 CREATE TABLE `AppUser` (
   `idUser` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `nameUser` varchar(100) UNIQUE NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `email` varchar(100) UNIQUE NOT NULL,
   `idRole` int NOT NULL
 );
 
 CREATE TABLE `Role` (
   `idRole` int PRIMARY KEY AUTO_INCREMENT,
-  `nameRol` varchar(100) NOT NULL
+  `roleName` varchar(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE `Song` (
+  `idSong` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `songName` varchar(100) NOT NULL,
+  `filePath` VARCHAR(255) NOT NULL,
+  `durationSeconds` int NOT NULL,
+  `releaseDate` DATETIME NOT NULL,
+  `idSongGenre` int NOT NULL,
+  `idAppUser` int NOT NULL
+);
+
+CREATE TABLE `Visualization` (
+  `idVisualizations` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `playCount` int NOT NULL,
+  `period` DATE NOT NULL,
+  `idSong` int NOT NULL
+);
+
+CREATE TABLE `SongGenre` (
+  `idSongGenre` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `genreName` VARCHAR(100)
 );
 
 ALTER TABLE `AppUser` ADD FOREIGN KEY (`idRole`) REFERENCES `Role` (`idRole`);
+
+ALTER TABLE `Song` ADD FOREIGN KEY (`idSongGenre`) REFERENCES `SongGenre` (`idSongGenre`);
+
+ALTER TABLE `Song` ADD FOREIGN KEY (`idAppUser`) REFERENCES `AppUser` (`idUser`);
+
+ALTER TABLE `Visualization` ADD FOREIGN KEY (`idSong`) REFERENCES `Song` (`idSong`);
--- 02-init-data.sql
+++ 02-init-data.sql
@@ -1,8 +1,42 @@
 -- default roles
-INSERT INTO `Role` (`nameRol`) VALUES ('listener'), ('moderator');
+INSERT INTO `Role` (`roleName`) VALUES ('listener'), ('moderator');
 
 
 
 -- U
 INSERT INTO soundnest.AppUser (nameUser, password, email, idRole)
-VALUES ('unaay20', '$2a$12$JBU0e5bDpDjdhNipE.k.a.hIXReLMe3qcuJ0h3GJZGNvlVi7DlGti', 'unaayjose@gmail.com', '2');
+VALUES ('unaay20', '$2a$12$JBU0e5bDpDjdhNipE.k.a.hIXReLMe3qcuJ0h3GJZGNvlVi7DlGti', 'unaayjose@gmail.com', '2'),
+('1', '$2a$10$4EhEyAirIGdY.FShS0LX0.rvuMn5IdJYeKoClVMWSVlFVexBkZyY6', 'zs22013698@estudiantes.uv.mx', '2')
+;
+
+INSERT INTO SongGenre (genreName) VALUES
+('Pop'),
+('Rock'),
+('Jazz'),
+('Blues'),
+('Classical'),
+('Hip Hop / Rap'),
+('R&B'),
+('Soul'),
+('Funk'),
+('Disco'),
+('Country'),
+('Reggae'),
+('Electronic / Dance'),
+('Salsa'),
+('Merengue'),
+('Bachata'),
+('Cumbia'),
+('Reggaetón'),
+('Folk'),
+('Metal'),
+('Indie'),
+('World Music'),
+('Banda'),
+('Corridos Tumbados'),
+('Trap'),
+('K-pop'),
+('Latin Pop'),
+('Punk'),
+('Alternative'),
+('Lo-fi');
