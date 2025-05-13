949a856ea667ea2096c6ac087f43e68b88d3cc89
Navi <168604098+King-Navi@users.noreply.github.com>
2025-05-13 00:47:03 -0600

    Added a song for testing

--- 02-init-data.sql
+++ 02-init-data.sql
@@ -1,12 +1,9 @@
--- default roles
 INSERT INTO `Role` (`roleName`) VALUES ('listener'), ('moderator');
 
-
-
--- U
 INSERT INTO soundnest.AppUser (nameUser, password, email, idRole)
 VALUES ('unaay20', '$2a$12$JBU0e5bDpDjdhNipE.k.a.hIXReLMe3qcuJ0h3GJZGNvlVi7DlGti', 'unaayjose@gmail.com', '2'),
-('1', '$2a$10$4EhEyAirIGdY.FShS0LX0.rvuMn5IdJYeKoClVMWSVlFVexBkZyY6', 'zs22013698@estudiantes.uv.mx', '2')
+('1', '$2a$10$4EhEyAirIGdY.FShS0LX0.rvuMn5IdJYeKoClVMWSVlFVexBkZyY6', 'zs22013698@estudiantes.uv.mx', '1'),
+('2', '$2a$10$SoDa1adqsa1wT7VDNmJ7o.EfTsjB2xSsIMolrEItNmR8gIBzvlP/i', 'zs22013698aa@estudiantes.uv.mx', '2')
 ;
 
 INSERT INTO SongGenre (genreName) VALUES
@@ -44,3 +41,23 @@ INSERT INTO SongGenre (genreName) VALUES
 INSERT INTO SongExtension (extensionName) VALUES
 ('mp3'),
 ('wav');
+
+
+INSERT INTO Song (
+  songName,
+  fileName,
+  durationSeconds,
+  releaseDate,
+  idSongGenre,
+  idSongExtension,
+  idAppUser
+)
+VALUES (
+  'Canción de Prueba',
+  'cancion_prueba.mp3',
+  180, -- duración en segundos
+  NOW(), -- fecha de lanzamiento
+  1, -- id del género (asegúrate que coincida)
+  1, -- id de la extensión (asegúrate que coincida)
+  2  -- id del usuario (asegúrate que coincida)
+);
